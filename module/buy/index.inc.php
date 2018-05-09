b75cf73cd5a67bc051916c8b9f414d26716dc03d
Socott <yidai@longsgo.com>
2018-05-09 15:12:31 +0800

    求购首页

--- module/buy/index.inc.php
+++ module/buy/index.inc.php
@@ -1,6 +1,90 @@
 <?php
 defined('IN_DESTOON') or exit('Access Denied');
 require DT_ROOT.'/module/'.$module.'/common.inc.php';
+//所有分类
+global $db;
+$rs = $db -> query("select catid,catname from {$db->pre}category where moduleid=$moduleid and parentid=0");
+while ($r = $db->fetch_array($rs)){
+    $category[] = $r;
+}
+$empfilter = isset($empfilter) ? $empfilter : '0';
+$comTypefilter = isset($comTypefilter) ? $comTypefilter : '0';
+$addtime = isset($addtime) ? $addtime : 'desc';
+
+$condition = 'status=3 ';
+if($catid) {//分类
+    $condition .= $CAT['child'] ? " AND catid IN (".$CAT['arrchildid'].")" : " AND catid=$catid";
+}
+if($areaid) {//地区
+    $condition .= $ARE['child'] ? " AND areaid IN (" . $ARE['arrchildid'] . ")" : " AND areaid=$areaid";
+}
+
+if($empfilter){//企业规模
+    $r = $db -> query("select company from {$db->pre}company where size='$empfilter'");
+    while($rs = $db->fetch_array($r)){
+        $companys[] = $rs;
+    }
+    foreach ($companys as $v){
+        $company .= ",'".$v['company']."'";
+    }
+    $company = substr($company,1);
+    $condition .= " AND company IN ($company)";
+}
+if($comTypefilter){//企业类型
+    $r = $db -> query("select company from {$db->pre}company where mode='$comTypefilter'");
+    while($rs = $db->fetch_array($r)){
+        $companys[] = $rs;
+    }
+    foreach ($companys as $v){
+        $company .= ",'".$v['company']."'";
+    }
+    $company = substr($company,1);
+    $condition .= " AND company IN ($company)";
+}
+if($addtime){
+    $MOD['order'] = " addtime $addtime";
+}
+
+$member_set = include DT_ROOT.'/file/setting/module-2.php';
+$com_size = explode('|', $member_set['com_size']);//企业规模
+$com_type = explode('|', $member_set['com_type']);//企业类型
+
+//求购信息条数
+$t1 = microtime(true);
+$items = $db->count($table, $condition, $CFG['db_expires']);
+//$number = $db -> get_one("select count(itemid) as number from {$db->pre}buy_6 where  $condition");
+$t2 = microtime(true);
+$usetime = round(($t2-$t1)*1000,2);
+
+//分页
+$pagesize = $MOD['pagesize'];//每页
+$offset = ($page-1)*$pagesize;//每页开始
+$total = ceil($items/$pagesize);//总页数
+if($page < 1 || $page > $total) $page = 1;
+
+//$pages = listpages($CAT, $items, $page, $pagesize);
+//var_dump($pages);
+$tags = array();
+$result = $db->query("SELECT * FROM {$table} WHERE {$condition} ORDER BY ".$MOD['order']." LIMIT {$offset},{$pagesize}", ($CFG['db_expires'] && $page == 1) ? 'CACHE' : '', $CFG['db_expires']);
+while($r = $db->fetch_array($result)) {
+//    $r['adddate'] = timetodate($r['addtime'], 5);
+//    $r['editdate'] = timetodate($r['edittime'], 5);
+    //if($lazy && isset($r['thumb']) && $r['thumb']) $r['thumb'] = DT_SKIN.'image/lazy.gif" original="'.$r['thumb'];
+    $r['alt'] = $r['title'];
+    $r['catname']  = $db->get_one("select catname from {$db->pre}category where catid=".$r['catid']);
+    $r['title'] = set_style($r['title'], $r['style']);
+    $r['linkurl'] = $MOD['linkurl'].$r['linkurl'];
+    $tags[] = $r;
+}
+
+//所有地区
+$r = $db -> query("select areaid,areaname from {$db->pre}area where parentid=0");
+while ($rs = $db->fetch_array($r)){
+    $area[] = $rs;
+}
+
+
+
 if(!check_group($_groupid, $MOD['group_index'])) include load('403.inc');
 $typeid = isset($typeid) ? intval($typeid) : 99;
 isset($TYPE[$typeid]) or $typeid = 99;
